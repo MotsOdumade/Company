309fc80bb2a8e261f7e5df18cb02902e8bd2c790
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-05-13 01:48:26 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -240,6 +240,13 @@ async function top_employees_request(){
     for (let i = 0; i < queryData.length; i++){
       company_data["labels"].push(queryData[i]["first_name"]);
       // completed tasks for that user in the past month
+          if (i == 0){
+                query2 += `SELECT COUNT(*) as num_tasks  FROM task   LEFT JOIN task_start ON task.id = task_start.task_id   WHERE task_start.task_id IS NULL AND  deadline > STR_TO_DATE('2024-05-17 13:42:04', '%Y-%m-%d %H:%i:%s') AND assigned_user_id = ${queryData[i]["assigned_user_id"]}`;
+     
+          } else {
+                query2 += ` UNION ALL SELECT COUNT(*) as num_tasks  FROM task   LEFT JOIN task_start ON task.id = task_start.task_id   WHERE task_start.task_id IS NULL AND  deadline > STR_TO_DATE('2024-05-17 13:42:04', '%Y-%m-%d %H:%i:%s') AND assigned_user_id = ${queryData[i]["assigned_user_id"]}`;
+     
+          }
      query2 += `SELECT COUNT(*) as num_tasks  FROM task   LEFT JOIN task_start ON task.id = task_start.task_id   WHERE task_start.task_id IS NULL AND  deadline > STR_TO_DATE('2024-05-17 13:42:04', '%Y-%m-%d %H:%i:%s') AND assigned_user_id = ${queryData[i]["assigned_user_id"]}`;
      // in progress tasks for that user
      query2 += ` UNION ALL SELECT COUNT(*) as num_tasks  FROM task   INNER JOIN task_start ON task.id = task_start.task_id   LEFT JOIN task_complete ON task.id = task_complete.task_id   WHERE task_complete.task_id IS NULL AND deadline > STR_TO_DATE('2024-05-17 13:42:04', '%Y-%m-%d %H:%i:%s') AND assigned_user_id = ${queryData[i]["assigned_user_id"]}`;
